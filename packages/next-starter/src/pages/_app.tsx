f44a18ad691e9b27d69da61035d680296c2b522d
Kos <koshuang@gmail.com>
2021-05-02 00:47:39 +0800

    refactor(share): add migrage index

--- packages/next-starter/src/pages/_app.tsx
+++ packages/next-starter/src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { makeServer } from '@onr/shared/mirage/mirage';
+import { makeServer } from '@onr/shared/mirage';
 
 if (process.env.NODE_ENV === 'development' && process.env.ENABLE_MIRAGE === 'true') {
   makeServer({ environment: 'development' });
