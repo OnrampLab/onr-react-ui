3d7ecfabba4d5a4ba0d9188c76dbbfcbf08f6c56
Kos <koshuang@gmail.com>
2021-06-17 00:29:39 +0800

    feat(core/wrapper): [createApp] added

---
+++ packages/onr-core/src/wrappers/createApp.tsx
@@ -0,0 +1,22 @@
+import { App } from '../components';
+import { AppOptions } from '../types';
+
+/**
+ * Creates a new Backstage App.
+ */
+export function createApp(options?: AppOptions) {
+  // TODO: should create a DefaultNotFoundPage
+  const DefaultNotFoundPage = () => <div>PAGE NOT FOUND</div>;
+
+  const components = {
+    NotFoundErrorPage: DefaultNotFoundPage,
+    ...options?.components,
+  };
+
+  const app = new App({
+    components,
+    routes: options?.routes,
+  });
+
+  return app;
+}
