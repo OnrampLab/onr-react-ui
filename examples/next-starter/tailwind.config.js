2b1d3d0a7e3cb58d66afb52a5551facf0f55f0b2
Kos <koshuang@gmail.com>
2021-07-06 00:40:36 +0800

    fix(starter): original tailwindcss theme colors are overrided

--- examples/next-starter/tailwind.config.js
+++ examples/next-starter/tailwind.config.js
@@ -26,9 +26,10 @@ module.exports = {
   purge: ['./src/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-    colors: {
-      ...customColors,
+    extend: {
+      colors: {
+        ...customColors,
+      },
     },
   },
   variants: {
