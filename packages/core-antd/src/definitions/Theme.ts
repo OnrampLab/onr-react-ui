a8dfb1203c7273dd0cbfa2469d02616708bdd7ad
Kos <koshuang@gmail.com>
2021-07-17 17:55:17 +0800

    chore(core-antd/definition): [Theme] new

---
+++ packages/core-antd/src/definitions/Theme.ts
@@ -0,0 +1,15 @@
+export type Theme = {
+  primaryColor: string;
+  normalColor: string;
+  backgroundColor: string;
+  darkColor: string;
+  textColor: string;
+  textColorSecondary: string;
+  successColor: string;
+  infoColor: string;
+  warningColor: string;
+  errorColor: string;
+  codeFamily: string;
+  borderColorBase: string;
+  borderRadiusBase: string;
+};
--- packages/core-antd/src/index.ts
+++ packages/core-antd/src/index.ts
@@ -1,2 +1,3 @@
 export * from './components';
+export * from './definitions';
 export * from './pages';
