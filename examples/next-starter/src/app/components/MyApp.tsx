a3b7644c8dc4729dee17668e0c1e5e19a39cfa22
Kos <koshuang@gmail.com>
2021-07-25 23:45:34 +0800

    feat(starter/component): [MyApp] add next-auth provider

--- examples/next-starter/src/app/components/MyApp.tsx
+++ examples/next-starter/src/app/components/MyApp.tsx
@@ -1,7 +1,7 @@
 import { createApp, OnrApp } from '@onr/core';
 import { Header, Page, SidebarMenu } from '@onr/core-antd';
+import { Provider } from 'next-auth/client';
 import { Context, createWrapper, MakeStore } from 'next-redux-wrapper';
-import { AppContext } from 'next/app';
 import Head from 'next/head';
 import Router from 'next/router';
 import { done, start } from 'nprogress';
@@ -73,28 +73,49 @@ export function AppComponent(props: Props) {
           <script src="https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/7.2.5/polyfill.min.js" />
         )}
       </Head>
-      <AppProvider>
-        <PageContainer {...props} />
-      </AppProvider>
+      <Provider
+        // Provider options are not required but can be useful in situations where
+        // you have a short session maxAge time. Shown here with default values.
+        options={{
+          // Client Max Age controls how often the useSession in the client should
+          // contact the server to sync the session state. Value in seconds.
+          // e.g.
+          // * 0  - Disabled (always use cache value)
+          // * 60 - Sync session state with server if it's older than 60 seconds
+          clientMaxAge: 0,
+          // Keep Alive tells windows / tabs that are signed in to keep sending
+          // a keep alive request (which extends the current session expiry) to
+          // prevent sessions in open windows from expiring. Value in seconds.
+          //
+          // Note: If a session has expired when keep alive is triggered, all open
+          // windows / tabs will be updated to reflect the user is signed out.
+          keepAlive: 0,
+        }}
+        session={pageProps.session}
+      >
+        <AppProvider>
+          <PageContainer {...props} />
+        </AppProvider>
+      </Provider>
     </>
   );
 }
 
-// NOTE: In order to get runtime config. We will need to use getInitialProps. But the down side
-//       is it will opt out Next.js default static optimization.
-// Please refer to https://nextjs.org/docs/api-reference/next.config.js/runtime-configuration
-AppComponent.getInitialProps = async ({ Component, ctx }: AppContext) => {
-  // Keep in mind that this will be called twice on server, one for page and second for error page
-  // ctx.store.dispatch({ type: "DEMO.SET_PLANT_IMAGE", payload: { demos:[] } });
+// // NOTE: In order to get runtime config. We will need to use getInitialProps. But the down side
+// //       is it will opt out Next.js default static optimization.
+// // Please refer to https://nextjs.org/docs/api-reference/next.config.js/runtime-configuration
+// AppComponent.getInitialProps = async ({ Component, ctx }: AppContext) => {
+//   // Keep in mind that this will be called twice on server, one for page and second for error page
+//   // ctx.store.dispatch({ type: "DEMO.SET_PLANT_IMAGE", payload: { demos:[] } });
 
-  return {
-    pageProps: {
-      // Call page-level getInitialProps
-      ...(Component.getInitialProps ? await Component.getInitialProps(ctx) : {}),
-      // Some custom thing for all pages
-      appProp: ctx.pathname,
-    },
-  };
-};
+//   return {
+//     pageProps: {
+//       // Call page-level getInitialProps
+//       ...(Component.getInitialProps ? await Component.getInitialProps(ctx) : {}),
+//       // Some custom thing for all pages
+//       appProp: ctx.pathname,
+//     },
+//   };
+// };
 
 export const MyApp = wrapper.withRedux(AppComponent);
