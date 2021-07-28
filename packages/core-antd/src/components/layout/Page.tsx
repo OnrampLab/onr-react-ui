89c01d45530912338459b8bfaf76f873dbfe0c38
Kos <koshuang@gmail.com>
2021-07-29 02:39:37 +0800

    feat(core-antd/component): [Page] use useAuth hook to handle user

--- packages/core-antd/src/components/layout/Page.tsx
+++ packages/core-antd/src/components/layout/Page.tsx
@@ -1,9 +1,8 @@
-import { AppContext, Container, CoreStore, Inner } from '@onr/core';
-import { Layout, Spin } from 'antd';
+import { AppContext, Container, CoreStore, Inner, useAuth } from '@onr/core';
+import { Layout } from 'antd';
 import { useRouter } from 'next/router';
-import { FC, useContext, useEffect, useState } from 'react';
+import { FC, useContext } from 'react';
 import { useSelector } from 'react-redux';
-import { AnyAction } from 'redux';
 import { ThemeProvider } from 'styled-components';
 import { Header } from './Header';
 import { SidebarMenu } from './SidebarMenu';
@@ -12,51 +11,42 @@ interface Props {
   children: JSX.Element;
   theme: any;
   HeaderMainSection: FC;
-  logout: () => AnyAction;
 }
 
 const { Content } = Layout;
 
 /* eslint-disable complexity */
 export const Page = (props: Props) => {
-  const { HeaderMainSection, theme, logout, children } = props;
+  const { HeaderMainSection, theme, children } = props;
   const router = useRouter();
   const appConfig = useContext(AppContext)?.getAppConfig();
   const fullPageRoutes = appConfig.fullPageRoutes;
-  const currentUser = useSelector((store: CoreStore) => store.authStore.currentUser);
+  //@ts-ignore
+  const { user } = useAuth();
+
   const { boxed, darkSidebar, sidebarPopup, weakColor } = useSelector(
     (store: CoreStore) => store.coreStore,
   );
-  const [loading, setLoading] = useState(true);
   const isNotDashboard = router && fullPageRoutes.includes(router.pathname);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 1000);
-  }, [loading]);
-
   return (
-    <Spin tip="Loading..." size="large" spinning={loading}>
-      <ThemeProvider theme={theme}>
-        <Container className={`${weakColor ? 'weakColor' : ''} ${boxed ? 'boxed shadow-sm' : ''}`}>
-          {!isNotDashboard && <Header HeaderMainSection={HeaderMainSection} />}
-          <Layout className="workspace">
-            {!isNotDashboard && (
-              <SidebarMenu
-                logout={logout}
-                currentUser={currentUser}
-                sidebarTheme={darkSidebar ? 'dark' : 'light'}
-                sidebarMode={sidebarPopup ? 'vertical' : 'inline'}
-              />
-            )}
+    <ThemeProvider theme={theme}>
+      <Container className={`${weakColor ? 'weakColor' : ''} ${boxed ? 'boxed shadow-sm' : ''}`}>
+        {!isNotDashboard && <Header HeaderMainSection={HeaderMainSection} />}
+        <Layout className="workspace">
+          {!isNotDashboard && (
+            <SidebarMenu
+              currentUser={user}
+              sidebarTheme={darkSidebar ? 'dark' : 'light'}
+              sidebarMode={sidebarPopup ? 'vertical' : 'inline'}
+            />
+          )}
 
-            <Layout>
-              <Content>{!isNotDashboard ? <Inner>{children}</Inner> : children}</Content>
-            </Layout>
+          <Layout>
+            <Content>{!isNotDashboard ? <Inner>{children}</Inner> : children}</Content>
           </Layout>
-        </Container>
-      </ThemeProvider>
-    </Spin>
+        </Layout>
+      </Container>
+    </ThemeProvider>
   );
 };
