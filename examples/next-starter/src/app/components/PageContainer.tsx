0b6b76d9cf753d3bf8debd873c804f7b703b935f
Kos <koshuang@gmail.com>
2021-07-29 02:43:19 +0800

    feat(starter/comopnent): [PageComponent] no need to pass logout props

--- examples/next-starter/src/app/components/PageContainer.tsx
+++ examples/next-starter/src/app/components/PageContainer.tsx
@@ -1,4 +1,3 @@
-import { logout } from '@onr/auth';
 import { Page } from '@onr/core-antd';
 import { AccountSelector } from '@onr/plugin-account';
 import { AppProps } from 'next/app';
@@ -8,7 +7,7 @@ const Container: React.FC = (props: AppProps) => {
   const { Component, pageProps } = props;
 
   return (
-    <Page {...props} theme={theme} logout={logout} HeaderMainSection={AccountSelector}>
+    <Page {...props} theme={theme} HeaderMainSection={AccountSelector}>
       <Component {...pageProps} />
     </Page>
   );
