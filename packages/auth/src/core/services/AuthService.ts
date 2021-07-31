40f0486a4a8355e430a28fe060ca08262f58e303
Kos <koshuang@gmail.com>
2021-08-01 02:47:05 +0800

    feat(auth/service): [AuthService] add new method in order to be compatible with next-auth

--- packages/auth/src/core/services/AuthService.ts
+++ packages/auth/src/core/services/AuthService.ts
@@ -1,8 +1,15 @@
 import { Http } from '@onr/common';
 import { AuthUser } from '@onr/core';
+import { LoginPayload } from './interfaces';
 import { SigninPayload, SigninResponse, SignoutResponse } from './interfaces/AuthModel';
 
 export const AuthService = {
+  newLogin: async (credentials: LoginPayload) => {
+    return AuthService.login({
+      data: credentials,
+    });
+  },
+
   login: async (payload: SigninPayload): Promise<SigninResponse> => {
     try {
       const response = await Http.post<SigninResponse>('/auth/login', {
@@ -25,6 +32,13 @@ export const AuthService = {
       throw new Error(`Logout Error: ${error.message}`);
     }
   },
+
+  getUser: async (token: string) => {
+    Http.setToken(token);
+
+    return AuthService.getCurrentUser();
+  },
+
   getCurrentUser: async (): Promise<AuthUser> => {
     try {
       const response = await Http.post<AuthUser>('/auth/me');
