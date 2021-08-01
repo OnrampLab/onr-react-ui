22b065fdb5f048a7f97ae488cd858f18869559ab
Kos <koshuang@gmail.com>
2021-08-01 19:21:42 +0800

    chore(auth/mock): remove custom routes

--- packages/auth/src/__mocks__/index.ts
+++ packages/auth/src/__mocks__/index.ts
@@ -1,19 +1,12 @@
 import { Mock } from '@onr/mock';
-import { authRouteCallback } from './authRouteCallback';
 import { roleModel } from './roleModel';
 import roleSeeds from './roleSeeds.json';
-import authRoutes from './routes.json';
 
 export const mocks = [
   Mock.create({
     resource: 'role',
     seeds: roleSeeds,
     model: roleModel,
-  }),
-  Mock.create({
-    resource: 'auth',
-    routes: authRoutes,
-    routesCallback: authRouteCallback,
   }),
 ];
 
