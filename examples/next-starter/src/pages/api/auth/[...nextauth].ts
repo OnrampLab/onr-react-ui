0e2cdb07cdcb7943440dfda7a5f01d0ce732d1fb
Kos <koshuang@gmail.com>
2021-08-01 20:05:30 +0800

    feat(plugin/auth): create from auth package

--- examples/next-starter/src/pages/api/auth/[...nextauth].ts
+++ examples/next-starter/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,5 @@
-import { AuthService } from '@onr/auth';
 import { createNextAuthApi } from '@onr/core/api';
+import { AuthService } from '@onr/plugin-auth';
 
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
