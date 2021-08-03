4ec62aa0e6b508e24d92322275134e876e34aa90
Kos Huang <koshuang@gmail.com>
2021-08-04 00:26:26 +0800

    feat(core/provider): [PageProvider] add currentRoute

--- packages/core/src/providers/PageProvider.tsx
+++ packages/core/src/providers/PageProvider.tsx
@@ -5,6 +5,8 @@ import { useApp } from '../hooks';
 
 export interface IPageContext {
   isNotDashboard: boolean;
+  currentPath: string;
+  currentRoute: any;
 }
 
 export const PageContext = createContext<IPageContext | null>(null);
@@ -12,10 +14,20 @@ export const PageContext = createContext<IPageContext | null>(null);
 export const PageProvider = (props: any) => {
   const router = useRouter();
   const appConfig = useApp()?.getAppConfig();
+  const routes = useApp()?.getRoutes();
   const fullPageRoutes = appConfig?.fullPageRoutes;
 
   const isNotDashboard =
     router && fullPageRoutes?.some((routeRule: string) => minimatch(router.pathname, routeRule));
 
-  return <PageContext.Provider value={{ isNotDashboard }} {...props} />;
+  const currentRoute = routes.find((route: any) => {
+    return minimatch(router.pathname, route.path);
+  });
+
+  return (
+    <PageContext.Provider
+      value={{ isNotDashboard, currentPath: router.pathname, currentRoute }}
+      {...props}
+    />
+  );
 };
