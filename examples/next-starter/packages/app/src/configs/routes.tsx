65e9071abcca336e300877ab1bd3eb139a2e9c1e
Kos Huang <koshuang@gmail.com>
2021-08-08 23:30:27 +0800

    chore(starter/config): [routes] support adding LayoutContainer

--- examples/next-starter/packages/app/src/configs/routes.tsx
+++ examples/next-starter/packages/app/src/configs/routes.tsx
@@ -1,18 +1,34 @@
+import { createLayoutContainer } from '@onr/core';
+import dynamic from 'next/dynamic';
+
+const GlobalStyles = dynamic(() =>
+  import('../components/GlobalStyles').then(mod => mod.GlobalStyles),
+);
+const themePromise = import('../components/GlobalStyles').then(mod => mod.theme);
+
+const AntdLayoutContainer = createLayoutContainer({
+  GlobalStyles,
+  themePromise,
+});
+
 export const routes = [
   {
     path: '/admin',
     layout: 'antd-admin',
     authRequired: true,
+    LayoutContainer: AntdLayoutContainer,
   },
   {
     path: '/admin/*',
     layout: 'antd-admin',
     authRequired: true,
+    LayoutContainer: AntdLayoutContainer,
   },
   {
     path: '/auth/*',
     layout: 'antd-full-page',
     authRequired: true,
+    LayoutContainer: AntdLayoutContainer,
   },
   {
     path: '/_error',
