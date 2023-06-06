35b4b42b58c792bb307ab44ebe73cd5fbca79af0
Kos Huang <koshuang@gmail.com>
2023-06-06 16:31:52 +0800

    chore(script): [eslint-pr] fix when there is no matched files

--- examples/next-starter/tools/scripts/eslint-pr.sh
+++ examples/next-starter/tools/scripts/eslint-pr.sh
@@ -2,13 +2,13 @@
 set -e
 
 git fetch origin main:refs/remotes/origin/main
+files=$(git diff origin/main HEAD --name-only --diff-filter ACMR | egrep '\.(js|jsx|ts|tsx)$' || true)
 
-files=$(git diff origin/main HEAD --name-only --diff-filter ACMR | egrep '\.(js|jsx|ts|tsx)$')
-if [ -n "$files" ]; then
-    echo "$files" | xargs $(npm bin)/eslint --format junit -o reports/eslint/js-lint-results.xml
-    RESULT=$?
-
-  [ $RESULT -ne 0 ] && exit 1
+if [[ -n "$files" ]]; then
+  echo "$files" | xargs yarn eslint --format junit -o reports/eslint/js-lint-results.xml
 fi
 
+RESULT=$?
+
+[ $RESULT -ne 0 ] && exit 1
 exit 0
