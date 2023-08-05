d20fe7887c8c5aea987e8eb01e623aafb222c2a0
Kos Huang <koshuang@gmail.com>
2023-08-05 12:43:17 +0800

    chore(starter): add todos into menu

--- examples/next-starter/apps/web/src/configs/menuItems.tsx
+++ examples/next-starter/apps/web/src/configs/menuItems.tsx
@@ -7,27 +7,38 @@ export interface MenuItem {
   path?: string;
   icon?: ReactElement;
   children?: MenuItem[];
+  login: boolean;
   roles?: UserRoleName[];
 }
 
 export const menuItems: MenuItem[] = [
   {
+    path: '/todos',
+    name: 'TODO List',
+    login: false,
+    icon: <FiHome strokeWidth={1} size={16} />,
+  },
+  {
     path: '/admin',
     name: 'Dashboard',
+    login: true,
     icon: <FiHome strokeWidth={1} size={16} />,
   },
   {
     name: 'Admin',
     icon: <FiServer strokeWidth={1} size={16} />,
+    login: true,
     roles: [UserRoleName.SystemAdmin],
     children: [
       {
         path: '/admin/accounts',
         name: 'Accounts',
+        login: true,
       },
       {
         path: '/admin/users',
         name: 'Users',
+        login: true,
       },
     ],
   },
