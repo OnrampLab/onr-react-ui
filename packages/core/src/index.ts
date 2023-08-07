056abc1b50b516dd4fb806244694a9c02ee8e93a
Kos Huang <koshuang@gmail.com>
2023-08-07 16:33:06 +0800

    chore(core/usecase): [UseCase] add abstract class

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -7,4 +7,5 @@ export * from './pages';
 export * from './providers';
 export * from './redux';
 export * from './types';
+export * from './useCases';
 export * from './wrappers';
---
+++ packages/core/src/useCases/UseCase.ts
@@ -0,0 +1,3 @@
+export abstract class UseCase {
+  abstract perform(): any;
+}
