51830d47edaa11beda26bad8d9317bcc01fcdd54
Kos Huang <koshuang@gmail.com>
2023-08-07 16:34:20 +0800

    chore(next-starter): add todos link to home page

--- examples/next-starter/plugins/home/src/pages/HomeMainPage.tsx
+++ examples/next-starter/plugins/home/src/pages/HomeMainPage.tsx
@@ -1,4 +1,5 @@
 import { App } from '@onr/core';
+import Link from 'next/link';
 import React from 'react';
 
 export const HomeMainPage: React.FC = () => {
@@ -11,6 +12,9 @@ export const HomeMainPage: React.FC = () => {
   return (
     <>
       <h1>Welcome to next-starter</h1>
+      <div>
+        You can check out <Link href="/todos">Todos</Link>
+      </div>
     </>
   );
 };
