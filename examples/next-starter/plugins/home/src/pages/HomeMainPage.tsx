4754b2712a7eea8d681284b53aaf4bfaa196224f
Kos Huang <koshuang@gmail.com>
2023-08-09 12:52:01 +0800

    feat(starter/page): [HomeMainPage] add global modal example

--- examples/next-starter/plugins/home/src/pages/HomeMainPage.tsx
+++ examples/next-starter/plugins/home/src/pages/HomeMainPage.tsx
@@ -1,9 +1,17 @@
-import { App } from '@onr/core';
+import { App, useGlobalModal } from '@onr/core';
+import { Button } from 'antd';
 import Link from 'next/link';
 import React from 'react';
 
 export const HomeMainPage: React.FC = () => {
+  const { showModal } = useGlobalModal();
   const logger = App.getInstance().logger;
+
+  const openModal = () => {
+    showModal({
+      content: <div>This is a modal</div>,
+    });
+  };
 
   logger.debug('Page loaded', {
     name: 'HomeMainPage',
@@ -12,9 +20,16 @@ export const HomeMainPage: React.FC = () => {
   return (
     <>
       <h1>Welcome to next-starter</h1>
-      <div>
-        You can check out <Link href="/todos">Todos</Link>
-      </div>
+      <p>
+        You can check out <Link href="/todos">Todos</Link>.
+      </p>
+      <p>
+        Or test a{' '}
+        <Button onClick={openModal} type="primary">
+          global modal
+        </Button>
+        .
+      </p>
     </>
   );
 };
