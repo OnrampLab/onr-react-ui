fdc60a8a9e463927408dce7a052fd36a6f9c2c56
Kos Huang <koshuang@gmail.com>
2023-08-18 22:03:57 +0800

    refactor(antd/component): [Header] rewrite with menus

--- plugins/antd/src/components/layout/Header/Header.tsx
+++ plugins/antd/src/components/layout/Header/Header.tsx
@@ -1,23 +1,66 @@
 import { coreActions, CoreStore, useAuth } from '@onr/core';
-import { Avatar, Layout, Menu, Popconfirm } from 'antd';
+import { Avatar, Layout, Menu, MenuProps, Popconfirm } from 'antd';
 import Link from 'next/link';
-import React, { ReactNode } from 'react';
+import React, { ComponentType } from 'react';
 import { FiBarChart, FiSettings, FiTriangle } from 'react-icons/fi';
 import { useDispatch, useSelector } from 'react-redux';
 import { DashHeader } from './styles';
 
-const { SubMenu } = Menu;
-
 type Props = {
-  HeaderMainSection: ReactNode;
+  HeaderMainSection: ComponentType;
+};
+
+const UserMenu: React.FC = () => {
+  const dispatch = useDispatch();
+  const { user, signOut, signIn } = useAuth();
+
+  const items: MenuProps['items'] = [
+    {
+      key: 'submenu',
+      icon: <Avatar src="/static/images/avatar.jpg" />,
+      children: [
+        {
+          key: 'setting',
+          onClick: () => dispatch(coreActions.setOptionDrawer()),
+          icon: <FiSettings size={20} strokeWidth={1} />,
+        },
+        {
+          label: 'Profile',
+          key: 'profile',
+        },
+        {
+          type: 'divider',
+        },
+        {
+          label: (
+            <>
+              {user && (
+                <Popconfirm
+                  placement="top"
+                  title="Are you sure you want to sign out?"
+                  // TODO: should use client side render to improve UX
+                  onConfirm={() => signOut()}
+                  okText="Yes"
+                  cancelText="Cancel"
+                >
+                  <a>Sign Out</a>
+                </Popconfirm>
+              )}
+              {!user && <a onClick={() => signIn()}>Sign In</a>}
+            </>
+          ),
+          key: 'auth',
+        },
+      ],
+    },
+  ];
+
+  return <Menu mode="horizontal" items={items} />;
 };
 
 export const Header: React.FC<Props> = ({ HeaderMainSection }: Props) => {
-  const dispatch = useDispatch();
-  // @ts-ignore
-  const { signOut } = useAuth();
-
   const { name, mobile } = useSelector((store: CoreStore) => store.coreStore);
+  const dispatch = useDispatch();
 
   return (
     <DashHeader>
@@ -32,35 +75,10 @@ export const Header: React.FC<Props> = ({ HeaderMainSection }: Props) => {
           <strong className="mx-1 text-black">{name}</strong>
         </Link>
 
-        {/* @ts-ignore */}
         <HeaderMainSection />
 
         <span className="mr-auto" />
-        <Menu mode="horizontal">
-          <Menu.Item key="m1" onClick={() => dispatch(coreActions.setOptionDrawer())}>
-            <FiSettings size={20} strokeWidth={1} />
-          </Menu.Item>
-
-          <SubMenu key="submenu" title={<Avatar src="/static/images/avatar.jpg" />}>
-            <Menu.Item key="sub1">Profile</Menu.Item>
-            <Menu.Divider />
-            <Menu.Item key="sub2">
-              <a href="https://one-readme.fusepx.com">Help?</a>
-            </Menu.Item>
-            <Menu.Item key="sub3">
-              <Popconfirm
-                placement="top"
-                title="Are you sure you want to sign out?"
-                // TODO: should use client side render to improve UX
-                onConfirm={() => signOut()}
-                okText="Yes"
-                cancelText="Cancel"
-              >
-                <a>Sign Out</a>
-              </Popconfirm>
-            </Menu.Item>
-          </SubMenu>
-        </Menu>
+        <UserMenu />
       </Layout.Header>
     </DashHeader>
   );
