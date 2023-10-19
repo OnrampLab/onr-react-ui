cd881d35ac8073faa4d2f959744c6eeb251139a3
Kos Huang <koshuang@gmail.com>
2023-10-19 19:01:51 +0800

    chore(ts-rest-client): will remove empty string for list API call

--- packages/ts-rest-client/src/services/removeEmptyKeys.ts
+++ packages/ts-rest-client/src/services/removeEmptyKeys.ts
@@ -1,6 +1,6 @@
 export function removeEmptyKeys(obj: any) {
   for (const key in obj) {
-    if (obj[key] === null || obj[key] === undefined) {
+    if (obj[key] === null || obj[key] === undefined || obj[key] === '') {
       delete obj[key];
     } else if (typeof obj[key] === 'object') {
       removeEmptyKeys(obj[key]);
