73d6b34fa301510778d48b1bc1e5be223e7b5088
Laura Lee <l443018@gmail.com>
2023-11-16 12:28:13 +0800

    chore(plugin/antd): [createSiteGlobalStyles] move progress style to styles

--- plugins/antd/src/utils/createSiteGlobalStyles.ts
+++ plugins/antd/src/utils/createSiteGlobalStyles.ts
@@ -3,16 +3,6 @@ import { createGlobalStyle } from 'styled-components';
 
 export function createSiteGlobalStyles(theme: ThemeConfig) {
   const GlobalStyles = createGlobalStyle`
-  #nprogress .bar {
-    background: ${theme.primaryColor};
-  }
-  #nprogress .peg {
-    box-shadow: 0 0 10px ${theme.primaryColor}, 0 0 5px ${theme.primaryColor};
-  }
-  #nprogress .spinner-icon {
-    border-top-color: ${theme.primaryColor};
-    border-left-color: ${theme.primaryColor};
-  }
 `;
 
   return {
