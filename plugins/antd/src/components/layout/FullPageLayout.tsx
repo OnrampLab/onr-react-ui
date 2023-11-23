a0d4f68871e7faacfb2fa828d069ba6d47111560
Laura Lee <l443018@gmail.com>
2023-11-23 16:25:20 +0800

    chore(plugin/antd): [FullPageLayout] add Inner to fix page height problem

--- plugins/antd/src/components/layout/FullPageLayout.tsx
+++ plugins/antd/src/components/layout/FullPageLayout.tsx
@@ -1,16 +1,28 @@
 import { PageProps } from '@onr/core';
 import { Layout } from 'antd';
 import { FC } from 'react';
+import styled from 'styled-components';
 
 const { Content } = Layout;
 
+export const Inner = styled.div`
+  margin: 0 auto;
+  position: relative;
+  background-color: white;
+  height: 100vh;
+  overflow-y: auto;
+  overflow-x: hidden;
+`;
+
 export const FullPageLayout: FC<PageProps> = props => {
+  const { innerStyle } = props;
   const { children } = props;
+
   return (
-    <>
-      <Layout style={{ minHeight: '100vh' }}>
-        <Content>{children}</Content>
-      </Layout>
-    </>
+    <Layout>
+      <Content>
+        <Inner style={{ ...innerStyle }}>{children}</Inner>
+      </Content>
+    </Layout>
   );
 };
