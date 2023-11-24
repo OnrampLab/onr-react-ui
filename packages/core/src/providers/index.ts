74bcb9fcbd5a1cff45586a2c525a6207893c227f
Laura Lee <l443018@gmail.com>
2023-11-24 15:37:10 +0800

    chore(package/core): [RouteDynamicInjectedProviders] add

--- packages/core/src/providers/index.ts
+++ packages/core/src/providers/index.ts
@@ -2,4 +2,5 @@ export * from './AuthProvider';
 export * from './CustomMenuProvider';
 export * from './MenuItemsProvider';
 export * from './NextAuthProvider';
+export * from './RouteDynamicInjectedProviders';
 export * from './RouteProvider';
