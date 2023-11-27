2d3c088c9927426cccd9a5941d96d6189da8fc69
Laura Lee <l443018@gmail.com>
2023-11-27 18:35:19 +0800

    chore(plugin/todo): [TodoMenuToggle] add

---
+++ plugins/todo-demo-with-ts-crud/src/components/TodoMenuToggle.tsx
@@ -0,0 +1,13 @@
+import { FiBarChart } from 'react-icons/fi';
+
+export const todoMenuToggle = (
+  <FiBarChart
+    size={20}
+    strokeWidth={1}
+    color="var(--brand-primaryColor)"
+    style={{
+      transform: 'rotate(90deg)',
+      padding: '0 10px',
+    }}
+  />
+);
--- plugins/todo-demo-with-ts-crud/src/components/index.ts
+++ plugins/todo-demo-with-ts-crud/src/components/index.ts
@@ -1,3 +1,4 @@
 export * from './TodoDetails';
 export * from './TodoList';
 export * from './TodoLogo';
+export * from './TodoMenuToggle';
