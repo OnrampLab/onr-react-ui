c598e243a1e0eb74f7d4fcd5d7e09927a5771e20
Laura Lee <l443018@gmail.com>
2023-11-27 14:59:53 +0800

    chore(package/core): [Page] add RouteDynamicInjectedProviders to page

--- packages/core/src/components/layout/Page/Page.tsx
+++ packages/core/src/components/layout/Page/Page.tsx
@@ -1,24 +1,24 @@
-import { FC } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
-import { CustomMenuProvider, useRoute } from '../../../providers';
+import { RouteDynamicInjectedProviders, useRoute } from '../../../providers';
 import { CoreStore } from '../../../redux';
 import { PageProps } from '../../../types';
 import { App } from '../../App';
 import { Container } from './styles';
 
-/* eslint-disable complexity */
-export const Page: FC<PageProps> = props => {
+export const Page: React.FC<PageProps> = props => {
   const app = App.getInstance();
+  const { children, ...defaultPageProps } = props;
   const { currentRoute } = useRoute();
-  const LayoutComponent = app.getLayouts()[currentRoute.layout];
-
   const { boxed, weakColor } = useSelector((store: CoreStore) => store.coreStore);
+  const LayoutComponent = app.getLayouts()[currentRoute.layout];
+  const pageProps = { ...defaultPageProps, ...currentRoute.pageProps };
 
   return (
-    <CustomMenuProvider>
+    <RouteDynamicInjectedProviders currentRoute={currentRoute} defaultPageProps={defaultPageProps}>
       <Container className={`${weakColor ? 'weakColor' : ''} ${boxed ? 'boxed shadow-sm' : ''}`}>
-        <LayoutComponent {...props} />
+        <LayoutComponent {...pageProps}>{children}</LayoutComponent>
       </Container>
-    </CustomMenuProvider>
+    </RouteDynamicInjectedProviders>
   );
 };
