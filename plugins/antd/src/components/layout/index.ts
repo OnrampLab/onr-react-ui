65cd6a1a3fc2359abb43d0241c929de3815317ed
Laura Lee <l443018@gmail.com>
2023-12-08 14:59:14 +0800

    chore(plugin-antd): [index] export component

--- plugins/antd/src/components/layout/index.ts
+++ plugins/antd/src/components/layout/index.ts
@@ -1,4 +1,5 @@
 export * from './FullPageLayout';
 export * from './HeaderBarLeftSideMenuLayout';
 export * from './LeftSideMenuTopHeaderLayout';
+export * from './components';
 export * from './interfaces';
