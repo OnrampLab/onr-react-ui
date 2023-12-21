9eaec6e1acc10560fa5eaa895d3ed4acff581946
Laura Lee <l443018@gmail.com>
2023-12-21 10:32:00 +0800

    chore(plugin/todo): [TodoLoadingLogo] export logo as component and instance

--- plugins/todo-demo-with-ts-crud/src/components/TodoLoadingLogo.tsx
+++ plugins/todo-demo-with-ts-crud/src/components/TodoLoadingLogo.tsx
@@ -1,8 +1,10 @@
 import { LoadingOutlined } from '@ant-design/icons';
 
-export const todoLoadingLogo = (
+export const TodoLoadingLogo = () => (
   <div className="fixed top-0 bottom-0 left-0 right-0 flex items-center justify-center">
     <LoadingOutlined rev />
     Loading...
   </div>
 );
+
+export const todoLoadingLogo = <TodoLoadingLogo />;
